766297d927b6f4b8d3db9c59cb76ea9b07feb1fb
Balu-Kandru <swamy.kandru@gmail.com>
2025-02-02 16:15:49 +0530

    start

---
+++ api/cors.js
@@ -0,0 +1,38 @@
+export default async function handler(req, res) {
+    if (req.method === 'OPTIONS') {
+      res.setHeader('Access-Control-Allow-Origin', '*');
+      res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+      res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+      return res.status(200).end();
+    }
+  
+    const apiUrl = req.query.apiUrl;
+  
+    try {
+      const response = await fetch(apiUrl, {
+        method: 'GET',
+        headers: {
+          'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
+          'Referer': 'https://www.swiggy.com/',
+          'Accept': 'application/json',
+          'Connection': 'keep-alive',
+          'Accept-Encoding': 'gzip, deflate, br',
+        },
+      });
+  
+      if (!response.ok) {
+        throw new Error('Failed to fetch data');
+      }
+  
+      const data = await response.json();
+  
+      res.setHeader('Access-Control-Allow-Origin', '*');
+      res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+      res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  
+      res.status(200).json(data);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+      res.status(500).json({ error: 'Failed to fetch data from the API' });
+    }
+  }
