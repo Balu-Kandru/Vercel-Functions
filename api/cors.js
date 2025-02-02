57d5e56946450945b91b88ffd7dd02df4c296a00
Balu-Kandru <swamy.kandru@gmail.com>
2025-02-02 16:44:03 +0530

    clean up

--- api/cors.js
+++ api/cors.js
@@ -12,11 +12,7 @@ export default async function handler(req, res) {
       const response = await fetch(apiUrl, {
         method: 'GET',
         headers: {
-          'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
           'Referer': 'https://www.swiggy.com/',
-          'Accept': 'application/json',
-          'Connection': 'keep-alive',
-          'Accept-Encoding': 'gzip, deflate, br',
         },
       });
   
